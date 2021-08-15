c943b8a0c46e88667825d32f50def61cb580808f
ramprakash <ram.prakash@qure.ai>
2021-08-15 20:52:01 +0530

    ADD: water.sh file

---
+++ water.sh
@@ -0,0 +1,12 @@
+echo 1. what is in the directory
+ls -a
+echo
+echo 2. is git installed
+git --version
+echo 3. what is the workspace location
+echo $RUNNER_WORKSPACE
+echo 4. who is running the script
+whoami
+echo 5. how is the disk status
+df
+echo 6. the environment variables
